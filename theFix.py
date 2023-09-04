36df4f2df45085d1f8307ef277a1e7166c9911c1
Pigf00d <thesolarpulse@gmail.com>
2023-09-04 13:49:25 -0400

    Annotates every png file in the given directory and creates a new directory for the new png files

--- theFix.py
+++ theFix.py
@@ -1,49 +1,56 @@
 from lxml import etree
 import cv2
+import os
 
 
-#read in file
-path = r"C:\Users\theso\CSCI435\CSCI435-Project1\Programming-Assignment-Data\com.dropbox.android"
-
-with open(path + ".xml", "rb") as f:
-    file = f.read()
-
-root = etree.fromstring(file)
-
-#finding the bounds of every leaf node
-bounds = []
-
-for item in root.iter("*"):
-    if len(item) == 0:
-        #converting string to usable list of points
-        addition = item.attrib["bounds"]
-        addition = addition.replace("[", "")
-        addition = addition.replace("]", ",")
-        addition = addition.split(",")
-        bounds.append(addition[:-1])
+#Change path to whichever directory the xml and png files are in
+path = r"C:\Users\theso\CSCI435\CSCI435-Project1\Programming-Assignment-Data"
 
 
-print(bounds)
+
+xmlfiles = []
+for filename in os.listdir(path):
+    if filename.endswith(".xml"):
+        xmlfiles.append(filename)
 
 
-img = cv2.imread(path + ".png")
+for xmlfile in xmlfiles:
+    #print(type(item), item)
+    curFile =  path + "\\" + xmlfile
+
+    with open(curFile, "rb") as f:
+        file = f.read()
+
+    #setting up xml tree
+    root = etree.fromstring(file)
+
+    #finding the bounds of every leaf node
+    bounds = []
+
+    for item in root.iter("*"):
+        if len(item) == 0:
+            #converting string to usable list of points
+            addition = item.attrib["bounds"]
+            addition = addition.replace("[", "")
+            addition = addition.replace("]", ",")
+            addition = addition.split(",")
+            bounds.append(addition[:-1])
 
 
-for item in bounds:
-    minX = min(int(item[2]), int(item[3]))
-    maxX = max(int(item[2]), int(item[3]))
-    minY = min(int(item[0]), int(item[1]))
-    maxY = max(int(item[0]), int(item[1]))
+    img = cv2.imread(curFile[:-4] + ".png")
 
-    cv2.rectangle(img, (int(item[0]), int(item[1])), (int(item[2]), int(item[3])), (0, 255, 255), 5)
 
-	
-cv2.imshow('image', img)
+    for item in bounds:
+        minX = min(int(item[2]), int(item[3]))
+        maxX = max(int(item[2]), int(item[3]))
+        minY = min(int(item[0]), int(item[1]))
+        maxY = max(int(item[0]), int(item[1]))
 
-cv2.imwrite(path + "WithBorders.png", img)
+        cv2.rectangle(img, (int(item[0]), int(item[1])), (int(item[2]), int(item[3])), (0, 255, 255), 5)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+        
+
+    cv2.imwrite(r"C:\Users\theso\CSCI435\CSCI435-Project1\annotatedScreenshots\\" + xmlfile[:-4] + ".png", img)
 
 
 
